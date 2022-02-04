856b3dd1bb56a1468d33471bed9147c04e5aff4d
Viviane Rocha <vlopes36@gmail.com>
2022-02-03 21:48:19 -0300

    AT

--- AnaliseExploratoriaDeDados.R
+++ AnaliseExploratoriaDeDados.R
@@ -1,8 +1,8 @@
 #Bootcamp - Cientista de dados - IGTI
 
-#Cápitulo 1 - Análise exploratória de dados com recursos nativos do R.
+#Capitulo 1 - Analise exploratoria de dados com recursos nativos do R.
 
-#Iniciaremos criando um dataframe contendo o histórico de vendas de café.
+#Iniciaremos criando um dataframe contendo o historico de vendas de cafe.
 
 dados <- data.frame(Vendas_Cafe = c(18, 20, 23, 23, 23, 23, 24, 25, 26, 26, 26, 26, 
                                      27, 28, 28, 
@@ -27,7 +27,7 @@ dados <- data.frame(Vendas_Cafe = c(18, 20, 23, 23, 23, 23, 24, 25, 26, 26, 26, 
 # Utilizaremos o comando head para vermos os primeiros dados do dataframe.
 head(dados)
 
-# Utilizaremos $ para selecionar variáveis específicas dentro do nosso conjunto de dados.
+# Utilizaremos $ para selecionar variaveis especificas dentro do nosso conjunto de dados.
 summary(dados$Preco_Cafe)
 #  Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 # 3.730   4.353   4.480   4.426   4.668   4.770 
@@ -40,64 +40,64 @@ summary(dados$Preco_Leite)
 #Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 #4.000   4.175   4.325   4.374   4.607   4.810
 
-# Utilizaremos o comando summary para verificarmos a estatística descritiva de todas as variáveis dos nossos dados.
+# Utilizaremos o comando summary para verificarmos a estatistica descritiva de todas as variaveis dos nossos dados.
 summary(dados)
 
-# Para verificarmos qual éo desvio padrão dos nossos dados, usaremos o comendo sd.
+# Para verificarmos qual e o desvio padrao dos nossos dados, usaremos o comendo sd.
 sd(dados$Preco_Cafe)
 # 0.3220568
-# Ou seja, o preço médio do café é 4.43 e o desvio padrão é 0.32.
+# Ou seja, o preco medio do cafe e 4.43 e o desvio padrao e 0.32.
 
 sd(dados$Vendas_Cafe)
 # 7.310833
-# A média de quantidade de unidades vendidas de café é 30.00 e o desvio padrão é 7.31
+# A media de quantidade de unidades vendidas de cafe e 30.00 e o desvio padrao ? 7.31
 
 sd(dados$Preco_Leite)
 # 0.2558082
-# O preço médio do leite é 4.37 e o desvio padrçao é 0.25
+# O pre?o medio do leite ? 4.37 e o desvio padraao ? 0.25
 
-# Utilizaremos um gráfico do tipo histograma para visualizarmos a variável Preco_Cafe
+# Utilizaremos um grafico do tipo histograma para visualizarmos a variavel Preco_Cafe
 hist(dados$Preco_Cafe)
 
 "Como customizar o histograma: 
 col = cor.
 border = contorno das colunas.
-main = Título principal do histograma.
-col.main = cor do título do gráfico.
-xlab = Título do eixo x.
-ylab = Título do eixo y."
+main = T?tulo principal do histograma.
+col.main = cor do t?tulo do gr?fico.
+xlab = T?tulo do eixo x.
+ylab = T?tulo do eixo y."
 
 hist(dados$Preco_Cafe,
      col = "DeepSkyBlue",border = "blue",
-     main = "Distribuição dos Preços Praticados para o café", col.main = "MidnightBlue",
-     xlab = "Preços praticados",
-     ylab = "Frequência")
+     main = "Distribuicao dos Precos Praticados para o cafe", col.main = "MidnightBlue",
+     xlab = "Pre?os praticados",
+     ylab = "Frequ?ncia")
 
-"Se quisermos ver todos os histogramas na mesma página, distribuídos em 2 linhas e 2 colunas, 
-precisamos utilizar o comando par para particionar os gráficos. Desta forma, conseguiremos analisar 
-os três histogramas em uma única visualização."
+"Se quisermos ver todos os histogramas na mesma pagina, distribuidos em 2 linhas e 2 colunas, 
+precisamos utilizar o comando par para particionar os gr?ficos. Desta forma, conseguiremos analisar 
+os tres histogramas em uma unica visualizacao."
 
 par(mfrow=c(2,2))
 hist(dados$Preco_Cafe,
      col = 'DeepSkyBlue', border = "blue",
-     main = "Distribuição dos Preços Praticados para o café", col.main = "MidnightBlue",
-     xlab = "Preços praticados",
-     ylab = "Frequência")
+     main = "Distribuicao dos Precos Praticados para o cafe", col.main = "MidnightBlue",
+     xlab = "Precos praticados",
+     ylab = "Frequencia")
 hist(dados$Vendas_Cafe,
      col = 'Aquamarine', border = "blue",
-     main = "Distribuição das vendas do Café", col.main = "MidnightBlue",
-     xlab = "Preços praticados",
-     ylab = "Frequência")
+     main = "Distribuicao das vendas do Cafe", col.main = "MidnightBlue",
+     xlab = "Precos praticados",
+     ylab = "Frequ?ncia")
 hist(dados$Preco_Leite,
      col = 'Orchid', border = "DeepPink",
-     main = "Distribuição das vendas do Leite", col.main = "MidnightBlue",
-     xlab = "Preços praticados",
-     ylab = "Frequência")
+     main = "Distribui??o das vendas do Leite", col.main = "MidnightBlue",
+     xlab = "Preccos praticados",
+     ylab = "Frequ?ncia")
 
-#Para sairmos deste modo de visualização, basta inserir o comando dev.off.
+#Para sairmos deste modo de visualizacao, basta inserir o comando dev.off.
 dev.off()
 
-# Relacionando os dados do preço do café com os dados de vendas do café.
+# Relacionando os dados do preco do cafe com os dados de vendas do cafe.
 plot(x = dados$Preco_Cafe,
      y = dados$Vendas_Cafe)
 
@@ -107,47 +107,47 @@ plot(x = dados$Preco_Cafe,
      y = dados$Vendas_Cafe,
      pch = 16,
      col = 'blue', border = "blue",
-     xlab = 'Preço',
+     xlab = 'Preco',
      ylab = "Quantidade vendida",
-     main = "Relação entre o preço e as vendas do café", col.main = "MidnightBlue",)
+     main = "Relacao entre o pre?o e as vendas do cafe", col.main = "MidnightBlue",)
 
-# Colorindo os pontos de quando teve promoção
+# Colorindo os pontos de quando teve promo??o
 plot(x = dados$Preco_Cafe,
      y = dados$Vendas_Cafe,
      pch = 16,
      col = factor(dados$Promocao),
-     xlab = 'Preço',
+     xlab = 'Preco',
      ylab = "Quantidade vendida",
-     main = "Relação entre o preço e as vendas do café")
+     main = "Relacao entre o preco e as vendas do cafe")
 
 # Add legenda
 legend(x=4.4, y=45,
-       c("Promoção", "Sem Promoção"),
+       c("Promocao", "Sem Promocao"),
        col = c("red", "black"),
        pch = c(16,16))
 
-# Para add linhas ao gráfico, utilize grid().
+# Para add linhas ao grafico, utilize grid().
 grid()
 
-# Criar uma nova variável que informará se a quantidade vendida está acima ou abaixo da média.
+# Criar uma nova variavel que informar? Se a quantidade vendida esta acima ou abaixo da media.
 media <- mean(dados$Vendas_Cafe)
 
-"Se as vendas do café estiverem acima da média, imprimir: Acima da Média, caso contrário, 
-imprimir: Abaixo da média"
+"Se as vendas do cafe estiverem acima da media, imprimir: Acima da Media, caso contrario, 
+imprimir: Abaixo da media"
 
 variavel <- ifelse(dados$Vendas_Cafe > media,
-                   "Acima da média",
-                   "Abaixo da média")
+                   "Acima da media",
+                   "Abaixo da media")
 
-# Transformar a variável em fator.
+# Transformar a variavel em fator.
 variavel <- factor(variavel)
 
-# Plotando a variável.
+# Plotando a variavel.
 plot(variavel)
 
-# Mostrar a quantidade de vendas abaixo da média e a quantidade de vendas acima da média.
+# Mostrar a quantidade de vendas abaixo da media e a quantidade de vendas acima da media.
 table(variavel)
-# Abaixo da média  Acima da média 
+# Abaixo da media  Acima da media 
 #19              11
 
 # Boxplot das vendas
@@ -155,19 +155,19 @@ par(mfrow=c(1,2))
 boxplot(dados$Vendas_Cafe,
         col = "Aquamarine", border = "blue",
         pch = 16,
-        main = "Dados das Vendas de Café", col.main = "MidnightBlue")
+        main = "Dados das Vendas de Cafe", col.main = "MidnightBlue")
 
-# Boxplot do preço do café
+# Boxplot do preco do cafe.
 boxplot(dados$Preco_Cafe,
         col = "Aquamarine", border = "blue",
         pch = 16,
-        main = "Dados do Preço do Café", col.main = "MidnightBlue")
+        main = "Dados do Pre?o do Cafe", col.main = "MidnightBlue")
 dev.off()
 
-# Comparando quando houve e quando não houve promoção.
+# Comparando quando houve e quando nao houve promocao.
 boxplot(dados$Vendas_Cafe~dados$Promocao,
         col = "blue", border = "black",
         pch = 16,
-        xlab = "Promoção",
+        xlab = "Promo??o",
         ylab = "Vendas",
-        main = "Vendas COM promoção vs Vendas SEM promoção",col.main = "MidnightBlue")
+        main = "Vendas COM promocao vs Vendas SEM promocao",col.main = "MidnightBlue")
